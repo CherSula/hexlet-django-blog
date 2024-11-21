ed3490c88331333c85c1a6c7afec6f5f493f781b
CherSula <chers1303@gmail.com>
2024-11-21 20:14:01 +0400

    step 7 views (class)

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hexlet_django_blog import views
+from hexlet_django_blog.views import HomePageView
+
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', HomePageView.as_view()),
     path('about/', views.about),
     path('articles/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class HomePageView(TemplateView):
+
+    self.template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        self.context = super().get_context_data(**kwargs)
+        self.context['who'] = 'World'
+        return self.context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
 
 
 def about(request):

57955c422227be1166a1cf9692dd767dd2ffbc96
CherSula <chers1303@gmail.com>
2024-12-03 13:28:41 +0400

    step 13 crud

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,28 +1,31 @@
 # from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.urls import reverse
+# from django.urls import reverse
 from hexlet_django_blog.article.models import Article
 
 
+# class Index(View):
+
+# def get(self, request, tags, article_id):
+#     # return HttpResponse('article')
+#     tags = ['статьи', 'articles', 'article', 'статья']
+#     return render(
+#         request,
+#         'index.html',
+#         context={'tags': tags, 'article_id': article_id},
+#     )
+
+# def home(self, request):
+#     return redirect(reverse('article', args=['python', 42]))
+
 class Index(View):
 
-    # def get(self, request, tags, article_id):
-    #     # return HttpResponse('article')
-    #     tags = ['статьи', 'articles', 'article', 'статья']
-    #     return render(
-    #         request,
-    #         'index.html',
-    #         context={'tags': tags, 'article_id': article_id},
-    #     )
-
-    # def home(self, request):
-    #     return redirect(reverse('article', args=['python', 42]))
-
-    def get(self, request):
-        articles = Article.objects.all()
-        print(articles)
-        return render(request, "articles/index.html", context={"articles": articles})
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
 
 
 class ArticleView(View):
